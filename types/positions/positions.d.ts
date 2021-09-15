bbdbf7cf318e3cc030e49042ce0b723c92e4d5d3
dmc0125 <domikmic@gmail.com>
2021-09-15 14:15:42 +0200

    Fix: types and build

--- types/positions/positions.d.ts
+++ types/positions/positions.d.ts
@@ -12,7 +12,7 @@ declare type Position = {
 };
 declare type ConfigParams = {
     API_SECRET: string;
-    STRATEGY: 'trend-signals';
+    STRATEGY: 'trend-signals' | 'trend-signals-v1';
     ENV: 'production' | 'backtest' | 'test';
     SIGNAL_PROVIDERS: SignalProviders;
     FUTURES: boolean;
