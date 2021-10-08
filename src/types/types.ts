086a3cda90f291d3bca7dce48bcbe9925a81e4d0
dmc0125 <domikmic@gmail.com>
2021-10-08 14:26:08 +0200

    Fix: fixed types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,5 +1,5 @@
 export type SignalProviders = ('zignaly' | 'compendium')[]
 
-export type Strategy = 'trading-signals' | 'trading-signals-v1'
+export type Strategy = 'trend-signals' | 'trend-signals-v1'
 
 export type ENVTypes = 'production' | 'backtest' | 'test'
